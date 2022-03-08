8c70159d2aa0af1e95cf82d496e5602f496da446
sw <sjugas1234@naver.com>
2022-03-08 21:04:45 +0900

    06 03/08/22

---
+++ src/main/kotlin/b10430.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    val a = Integer.parseInt(br.readLine())
+    val b = Integer.parseInt(br.readLine())
+
+    println("${a * (b % 10)}\n${a * ((b % 100) / 10)}\n${a * (b / 100)}\n${a*b}")
+}
